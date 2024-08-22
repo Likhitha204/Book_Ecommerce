2eb3db88f0eba14a1c369d8d3ee56ba3531a94ba
Likhitha-204 <blikhitha204@gmail.com>
2024-08-22 16:03:40 +0530

    run

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 
 mongoose.connect('mongodb://localhost:27017/book_store');
